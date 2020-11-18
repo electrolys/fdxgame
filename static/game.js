e620f887e2747a3834b05e8034d4dffb40cf186b
electrolys <47481369+electrolys@users.noreply.github.com>
2020-11-17 18:39:04 -0600

    Update game.js

--- static/game.js
+++ static/game.js
@@ -1,9 +1,6 @@
 var socket = io();
 
-var src = '//cdn.jsdelivr.net/npm/eruda';
-if (!/eruda=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
-document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
-document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
+
 // Load text with Ajax synchronously: takes path to file and optional MIME type
 function loadTextFileAjaxSync(filePath, mimeType)
 {
